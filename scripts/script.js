f525b6a99a3297fc02e2c508037fc89a951fdbc1
weiyuyu <weiyuyu@usc.edu>
2018-02-05 01:25:08 -0800

    Website Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-//Smooth Scrolling
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -15,7 +14,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1200, function(){
+      }, 1000, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
