a20bb6098247b202d89f04b908412b411c5bf320
weiyuyu <weiyuyu@usc.edu>
2018-06-16 14:34:22 +0800

    Added project modals

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -37,5 +37,65 @@ document.addEventListener('DOMContentLoaded', function(){
       }
     }
   });
-
 })(jQuery); // End of use strict
+
+//Modal
+document.querySelector('a#openStylecastModal').addEventListener('click', function(event) {
+  event.preventDefault();
+  var modal = document.querySelector('#stylecast-modal');  // assuming you have only 1
+  var html = document.querySelector('html');
+  modal.classList.add('is-active');
+  html.classList.add('is-clipped');
+
+  modal.querySelector('.modal-background').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+
+  modal.querySelector('.delete').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+});
+
+document.querySelector('a#openBotzyModal').addEventListener('click', function(event) {
+  event.preventDefault();
+  var modal = document.querySelector('#botzy-modal');  // assuming you have only 1
+  var html = document.querySelector('html');
+  modal.classList.add('is-active');
+  html.classList.add('is-clipped');
+
+  modal.querySelector('.modal-background').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+
+  modal.querySelector('.delete').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+});
+
+document.querySelector('a#openCryptoTrackerModal').addEventListener('click', function(event) {
+  event.preventDefault();
+  var modal = document.querySelector('#cryptotracker-modal');  // assuming you have only 1
+  var html = document.querySelector('html');
+  modal.classList.add('is-active');
+  html.classList.add('is-clipped');
+
+  modal.querySelector('.modal-background').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+
+  modal.querySelector('.delete').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+});
