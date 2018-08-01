3722f91d71884dc55964f3a2c4cf5d352d39d060
Wei-Yu (Wayne) Yu <weiyuyu@usc.edu>
2018-08-01 16:56:42 +0800

    Added new project and deleted hero portrait

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -40,6 +40,26 @@ document.addEventListener('DOMContentLoaded', function(){
 })(jQuery); // End of use strict
 
 //Modal
+document.querySelector('a#openBakeryModal').addEventListener('click', function(event) {
+  event.preventDefault();
+  var modal = document.querySelector('#bakery-modal');  // assuming you have only 1
+  var html = document.querySelector('html');
+  modal.classList.add('is-active');
+  html.classList.add('is-clipped');
+
+  modal.querySelector('.modal-background').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+
+  modal.querySelector('.delete').addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+  });
+});
+
 document.querySelector('a#openStylecastModal').addEventListener('click', function(event) {
   event.preventDefault();
   var modal = document.querySelector('#stylecast-modal');  // assuming you have only 1
